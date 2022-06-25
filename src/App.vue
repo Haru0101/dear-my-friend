cff925189c8593a81e817225ddab02c4ff7a03d9
Haru0101 <haruki.for.01@gmail.com>
2022-06-25 14:35:15 +0900

    feat: use ref

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { ref } from 'vue'
 
-const message = 'The accounting department has recently informed me that your sales reports have consistently been submitted after the deadlines. Please respond to this email with reasons as to why you have failed to meet the deadlines.';
+const message = ref('The accounting department has recently informed me that your sales reports have consistently been submitted after the deadlines. Please respond to this email with reasons as to why you have failed to meet the deadlines.');
 
-const to = 'Simon';
-const from = 'MarkBorthwick';
-const sentDate = 'February 8, 8:59 A.M.';
-const letterClosing = 'Regards';
-const letterGreeting = 'Dear';
+const to = ref('Simon');
+const from = ref('MarkBorthwick');
+const sentDate = ref('February 8, 8:59 A.M.');
+const letterClosing = ref('Regards');
+const letterGreeting = ref('Dear');
+
 </script>
 
 <template>
