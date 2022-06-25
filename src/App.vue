e70e435c86f33cdb8c5481fa4c6605ae873110e3
Haru0101 <haruki.for.01@gmail.com>
2022-06-25 14:32:02 +0900

    style: text align left

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,6 @@ const letterGreeting = 'Dear';
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
