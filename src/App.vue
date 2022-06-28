0835897afec0fa6ca93e3f4a52536470731500e4
Haru0101 <haruki.for.01@gmail.com>
2022-06-28 22:49:50 +0900

    Revert "refactor: extract form component"
    This reverts commit 346c29deba30dfe63412410ba9818d05598b61ff.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import { ref } from 'vue'
 import html2canvas from 'html2canvas'
 import LetterPreview from './components/LetterPreview.vue';
-import Form from './components/Form.vue';
 
 const message = ref('How are you doing? Sorry it took me too long to long to write back. How has summer been? What have you been doing during the summer? On my part, I have been traveling around the country with my family. I made new friends and new memories.\n\nI miss you. I cannot wait to go back to school and see you again. I also want to tell you about all my travel experiences when we are back in school. I will send you some of my pictures.\n\nI hope all is well with you. Send your family my love. See you soon.');
 
@@ -44,15 +43,32 @@ const download = () => {
           <span>Download</span>
         </button>
       </div>
-      <Form
-        v-model:to="to"
-        v-model:from="from"
-        v-model:sentDate="sentDate"
-        v-model:message="message"
-        v-model:letterGreeting="letterGreeting"
-        v-model:letterClosing="letterClosing"
-      >
-      </Form>
+      <div>
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" label for="to">宛先</label>
+          <input class="block shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="to" type="text" v-model="to">
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="from">送り主</label>
+          <input class="block shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="from" type="text" v-model="from">
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="sent_date">送付時刻</label>
+          <input class="block shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="sent_date" type="text" v-model="sentDate">
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="message">本文</label>
+          <textarea rows="6" class="block shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="message" type="text" v-model="message"></textarea>
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="letter_greeting">頭語</label>
+          <input class="block shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="letter_greeting" type="text" v-model="letterGreeting">
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="letter_closing">結語</label>
+          <input class="block shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="letter_closing" type="text" v-model="letterClosing">
+        </div>
+      </div>
     </div>
   </div>
 </template>
