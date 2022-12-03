b0a74e6c35bd65fcde90746b4b2dfc1fdea9cb09
Haru0101 <haruki.for.01@gmail.com>
2022-12-03 16:35:29 +0900

    fix: letter greeting visible

--- src/components/LetterPreview.vue
+++ src/components/LetterPreview.vue
@@ -11,7 +11,7 @@
 <template>
     <div class="mb-8 bg-white shadow-lg rounded">
         <div id="canvas-box" class="px-8 pt-6 pb-8">
-            <p class="mb-6">{{letterGreeting}} {{to}}</p>
+            <p class="mb-6"><span v-if="letterGreeting" class="inline-block">{{letterGreeting}}</span><span class="inline-block	 ml-1">{{to}}</span></p>
             <p class="mb-2 whitespace-pre-wrap">{{message}}</p>
             <p class="mb-6">{{letterClosing}}</p>
             <p class="flex justify-between"><span>{{from}}</span><span>{{sentDate}}</span></p>
